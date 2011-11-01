f343d193a05c94c86b263d1db7bc67e2a05e2e10
phoenix <hackingphoenix@gmail.com>
2011-11-01 19:12:47 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -61,5 +63,6 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
 end
